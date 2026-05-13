96a9b67538472f9f6b292e9b7a47d22eebda6af9
Rigved Thorat <rith8786@colorado.edu>
2026-05-13 17:34:42 -0600

    Untrack node_modules and build directories

--- backend/build/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const appServer_1 = require("./webSupport/appServer");
-const appConfig_1 = require("./appConfig");
-const environment_1 = require("./environment");
-appServer_1.appServer.start(8787, (0, appConfig_1.configureApp)(environment_1.environment.fromEnv()));
